57bc8b26bd6d6b1df1d68df2e40ef8fb88e17d8f
dogaruemiliano <dogaru.emiliano@gmail.com>
2022-08-15 23:33:14 +0200

    Added different client_id env var for development and production

--- constants/api.ts
+++ constants/api.ts
@@ -1,4 +1,6 @@
-export const CLIENT_ID = "3X_onJKKrCXR1gldoptCO6xD7jnHL0Qeqw4gN1JjKik"
+const CLIENT_ID_DEVELOPMENT = "3X_onJKKrCXR1gldoptCO6xD7jnHL0Qeqw4gN1JjKik"
+const CLIENT_ID_PRODUCTION = "tMiNaocDZvndPiVILavpj6UlZodpyFSw97BWO932geo"
+export const CLIENT_ID = process.env.NODE_ENV === 'development' ? CLIENT_ID_DEVELOPMENT : CLIENT_ID_PRODUCTION
 
 const BASE_URL_DEVELOPMENT = "http://localhost:3000/"
 const BASE_URL_PRODUCTION = "https://smart-home-dec.herokuapp.com/"
